ddecec7e55977edfa0299be3d08575398c383c12
Konstantin Kokhno <kkohno@mail.ru>
2016-07-06 01:29:52 +0600

    добавлен контроллер WindowsForms, для работы с моделью

--- CRM/CRM_VIEW/CRMController.Designer.cs
+++ CRM/CRM_VIEW/CRMController.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace CRM_VIEW
 {
-	partial class Form1
+	partial class CRMController
 	{
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
+		/// <summary> 
 		/// Освободить все используемые ресурсы.
 		/// </summary>
 		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
@@ -19,7 +19,7 @@
 			base.Dispose(disposing);
 		}
 
-		#region Код, автоматически созданный конструктором форм Windows
+		#region Код, автоматически созданный конструктором компонентов
 
 		/// <summary>
 		/// Требуемый метод для поддержки конструктора — не изменяйте 
@@ -27,28 +27,9 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.crmController1 = new CRM_VIEW.CRMController(this.components);
-			this.SuspendLayout();
-			// 
-			// crmController1
-			// 
-			this.crmController1.DBContext = null;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
-			this.Name = "Form1";
-			this.Text = "Главная";
-			this.ResumeLayout(false);
-
+			components = new System.ComponentModel.Container();
 		}
 
 		#endregion
-
-		private CRMController crmController1;
 	}
 }
-
---
+++ CRM/CRM_VIEW/CRMController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRM_MODEL;
+
+namespace CRM_VIEW
+{
+	public partial class CRMController : Component
+	{
+		CRMDBContext _dbContext;
+		public CRMDBContext DBContext
+		{
+			get
+			{
+				return _dbContext;
+			}
+			set
+			{
+				_dbContext = value;
+			}
+		}
+		User _user;
+		public CRM_MODEL.User User
+		{
+			get { return _user; }
+			private set
+			{
+				if (value == _user) return;
+				var last = _user;
+				_user = value;
+				if (last != null && OnDeauthorized != null) OnDeauthorized(this, last);
+				if (_user != null && OnAuthorized != null) OnAuthorized(this, _user);
+			}
+		}
+
+
+		public CRMController()
+		{
+			InitializeComponent();
+		}
+
+		public CRMController(IContainer container)
+		{
+			container.Add(this);
+
+			InitializeComponent();
+		}
+
+		[Category("Авторизация")]
+		[Description("Происходит когда был авторизован пользователь")]
+		public event EventHandler<User> OnAuthorized;
+		[Category("Авторизация")]
+		[Description("Происходит пользователь стал не авторизован")]
+		public event EventHandler<User> OnDeauthorized;
+	}
+}
